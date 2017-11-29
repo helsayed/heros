18a4aea9c72329b85272505fabdad5a3e99f3a6b
H Elsayed <eng.hussein.elsayed@gmail.com>
2017-11-29 19:11:18 +0200

    adding heros actions

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,15 @@
 class HerosController < ApplicationController
+  before_action :set_hero, only: [:show, :update, :destroy]
+  def index
+    @heros = Hero.all
+    json_response(@heros)
+  end
+   # GET /heros/:id
+  def show
+    json_response(@hero)
+  end
+  
+  def set_hero
+    @hero = Hero.find(params[:id])
+  end
 end

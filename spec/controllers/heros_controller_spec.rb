7d03fe32d7dc2b337b9b0a101c5de30e08ec44b6
H Elsayed <eng.hussein.elsayed@gmail.com>
2017-11-30 02:58:38 +0200

    remove non used specs

--- spec/controllers/heros_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe HerosController, type: :controller do
-
-end
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe Ability, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:description) }
-  it { should validate_presence_of(:is_ultimate) }
   it { should belong_to(:hero) }
 end

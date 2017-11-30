e2c2784bcbbfe538836b1a888f08a95ed58848c4
H Elsayed <eng.hussein.elsayed@gmail.com>
2017-11-30 03:08:48 +0200

    adding rake task for fetching

--- app/services/heros/fetching_heros_service.rb
+++ app/services/heros/fetching_heros_service.rb
@@ -8,12 +8,15 @@ module Heros
     end
 
     def execute
-      response = RestClient.get(fetch_url)
-      heros = JSON.parse(response.body)['data']
-      heros.each do |hero_raw|
-        Heros::CreateHeroService.new(hero_raw: hero_raw).execute
+      begin
+        response = RestClient.get(fetch_url)
+        heros = JSON.parse(response.body)['data']
+        heros.each do |hero_raw|
+          Heros::CreateHeroService.new(hero_raw: hero_raw).execute
+        end
+      rescue => e
+        puts e.response
       end
     end
-
   end
 end
